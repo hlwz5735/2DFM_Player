3c9f14d4b3a875285f3d42f6c6ffd88671403a14
Duke <limengwwwq@163.com>
2024-08-01 15:14:41 +0800

    feat: stage read 2

--- Source/game/StageTestScene.cpp
+++ Source/game/StageTestScene.cpp
@@ -22,8 +22,7 @@ bool StageTestScene::init() {
     auto &gameConfig = GlobalData::getInstance()->getGameConfig();
     auto kgt = GlobalData::getInstance()->getKgtGame();
 
-    auto stageName = std::format("{}/{}.stage", gameConfig.gameBasePath,
-         kgt->stageNames[1]);
+    auto stageName = std::format("{}/{}.stage", gameConfig.gameBasePath, kgt->stageNames[25]);
 
     KgtStage *stage = readStageFile(stageName);
     createTexturesForCommonResource(stage, 0);
@@ -53,6 +52,40 @@ bool StageTestScene::init() {
     }
 
     // scheduleUpdate();
+    auto mouseListener           = EventListenerMouse::create();
+    mouseListener->onMouseMove   = AX_CALLBACK_1(StageTestScene::onMouseMove, this);
+    mouseListener->onMouseUp     = AX_CALLBACK_1(StageTestScene::onMouseUp, this);
+    mouseListener->onMouseDown   = AX_CALLBACK_1(StageTestScene::onMouseDown, this);
+    _eventDispatcher->addEventListenerWithSceneGraphPriority(mouseListener, this);
+
+    setPosition(-320, 480);
 
     return true;
+}
+void StageTestScene::onMouseDown(ax::Event *event) {
+    auto e = static_cast<EventMouse *>(event);
+    if (e->getMouseButton() == EventMouse::MouseButton::BUTTON_RIGHT) {
+        ploc = e->getLocationInView();
+    }
+}
+void StageTestScene::onMouseUp(ax::Event *event) {
+    auto e = static_cast<EventMouse *>(event);
+    if (e->getMouseButton() == EventMouse::MouseButton::BUTTON_RIGHT) {
+        ploc = Vec2::ZERO;
+        loc = Vec2::ZERO;
+    }}
+void StageTestScene::onMouseMove(ax::Event *event) {
+    auto e = static_cast<EventMouse *>(event);
+    auto btn = e->getMouseButton();
+    if (btn == EventMouse::MouseButton::BUTTON_RIGHT) {
+        loc = e->getLocationInView();
+        Vec2 delta = loc - ploc;
+        AXLOG("delta: %.0f, %.0f", delta.x, delta.y);
+        auto oriPos = getPosition();
+        auto visibleSize = _director->getVisibleSize();
+        setPosition(
+            ax::clampf(oriPos.x + delta.x, -640, 0),
+            ax::clampf(oriPos.y + delta.y, 0, visibleSize.height)
+        );
+    }
 }
--- Source/game/StageTestScene.hpp
+++ Source/game/StageTestScene.hpp
@@ -12,8 +12,16 @@ struct KgtStage;
 class StageTestScene : public ax::Scene {
 public:
     bool init() override;
+
+    void onMouseDown(ax::Event* event);
+    void onMouseUp(ax::Event* event);
+    void onMouseMove(ax::Event* event);
+
 private:
     KgtStage *stage = nullptr;
+
+    ax::Vec2 ploc = ax::Vec2::ZERO;
+    ax::Vec2 loc = ax::Vec2::ZERO;
 };
 
 #endif //STAGETESTSCENE_HPP
